885e5bf9e7de38c529374628c7906cd3aca4f038
ayush <ayushyadav5867@gmail.com>
2022-11-05 14:46:27 +0530

    e-commerce

---
+++ front-end/src/components/Nav.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {Link , useNavigate} from 'react-router-dom'
+
+const Nav = ()=>{
+  const auth = localStorage.getItem("user");
+  const navigate = useNavigate();
+  const logout = ()=>{
+    localStorage.clear();
+    navigate('/signup')
+  }
+    return (
+      <div>
+      <img
+      alt='logo'
+      className='logo'
+       src='https://cdn-icons-png.flaticon.com/512/8437/8437926.png'  />
+      {auth ? <ul className="nav-ul">
+
+          <li><Link to="/">Product</Link></li>
+          <li><Link to="/add">Add Product</Link></li>
+          <li><Link to="/update">update Product</Link></li>
+          <li>  <Link to="/profile">Profile</Link></li>
+          <li><Link onClick={logout} to="/signup">Logout ({JSON.parse(auth).name})   </Link></li>
+          </ul>
+          :
+          <ul className='nav-ul nav-right'>
+           <li><Link to="/signup">Sign Up</Link></li>
+           <li><Link to="/login">Login</Link></li>
+           </ul>
+
+    }
+        
+      </div>
+    )
+}
+
+export default Nav;
